f34df653f4c320c3555ea675be5e484b459545f7
josephinehemingway <josephine.hemingway@gmail.com>
2021-03-05 02:00:54 +0800

    alternative code for auth but commented out

--- lib/auth.dart
+++ lib/auth.dart
@@ -4,7 +4,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn  googleSignIn = GoogleSignIn();
 
-Future<FirebaseUser> signInWithGoogle() async{
+Future<FirebaseUser> signIn() async{
+
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
 
@@ -23,6 +24,57 @@ Future<FirebaseUser> signInWithGoogle() async{
   return user;
 }
 
-void signOutGoogle() async{
+void signOut() async{
   await googleSignIn.signOut();
+  _auth.signOut();
+
+
+
 }
+//
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+//
+// class GoogleAuth extends ChangeNotifier {
+//
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final GoogleSignIn googleSignIn = GoogleSignIn();
+//   bool _isSigningIn;
+//
+//   GoogleAuth(){
+//     _isSigningIn = false;
+//   }
+//
+//   bool get isSigningIn => _isSigningIn;
+//
+//   set isSigningIn(bool isSigningIn) {
+//     _isSigningIn = isSigningIn;
+//     notifyListeners();
+//   }
+//
+//   Future signIn() async {
+//     isSigningIn = true;
+//
+//     final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+//
+//     if (GoogleSignInAccount == null) {
+//       isSigningIn = false;
+//       return;
+//     } else {
+//       final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount
+//           .authentication;
+//       final AuthCredential credential = GoogleAuthProvider.getCredential(
+//           idToken: googleSignInAuthentication.idToken,
+//           accessToken: googleSignInAuthentication.accessToken);
+//
+//       await _auth.signInWithCredential(credential);
+//       isSigningIn = false;
+//     }
+//   }
+//
+//   void signOut() async {
+//     await googleSignIn.signOut();
+//     _auth.signOut();
+//   }
+// }
