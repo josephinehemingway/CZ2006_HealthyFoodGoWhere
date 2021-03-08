e402b1b34912229b4a5f2b1684ceda90cafa3097
josephinehemingway <josephine.hemingway@gmail.com>
2021-03-08 23:03:11 +0800

    updated googlejson

--- lib/main.dart
+++ lib/main.dart
@@ -5,61 +5,35 @@ import 'package:flutter_app/theme.dart';
 import 'screens/Login/LoginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// void main() => runApp(MaterialApp(
-//       title: 'HealthyGoWhere App',
-//       theme: theme(),
-//       debugShowCheckedModeBanner: false,
-//       // home: BottomNavBar(),
-//       routes: routes,
-//       initialRoute: LoginPage.routeName,
-//     ));
-
 Future<void> main() async {
-      WidgetsFlutterBinding.ensureInitialized();
-      runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-      final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
-      @override
-      Widget build(BuildContext context) {
-            return MaterialApp(
-                title: 'HealthyGoWhere App',
-                theme: theme(),
-                debugShowCheckedModeBanner: false,
-                routes: routes,
-                initialRoute: LoginPage.routeName,
-                home: FutureBuilder(
-                    future: _fbApp,
-                    builder: (context, snapshot) {
-        if (snapshot.hasError) {
-        print('You have an error! ${snapshot.error
-            .toString()}');
-        return Text('Something went wrong!');
-        }
-        else if (snapshot.hasData) {
-        return LoginPage();
-        } else{
-        return Center(
-        child: CircularProgressIndicator(),
-        );
-        }
-        }
-        ));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'HealthyGoWhere App',
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+        routes: routes,
+        initialRoute: LoginPage.routeName,
+        home: FutureBuilder(
+            future: _fbApp,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print('You have an error! ${snapshot.error.toString()}');
+                return Text('Something went wrong!');
+              } else if (snapshot.hasData) {
+                return LoginPage();
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
+  }
 }
-      }
-
-//
-// class MyApp extends StatelessWidget {
-//       static final String title = 'HealthyGoWhere App';
-//
-//       @override
-//       Widget build(BuildContext context) => MaterialApp(
-//             debugShowCheckedModeBanner: false,
-//             title: title,
-//             theme: theme(),
-//             routes: routes,
-//             // initialRoute: LoginPage.routeName,
-//             home: LoginPage(),
-//       );
-// }
