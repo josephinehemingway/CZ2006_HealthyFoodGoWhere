8ec9bd532f6a15d4eabca2c2c206e216f794f2d7
chingfhen <tanchingfhen@gmail.com>
2021-03-14 21:55:42 +0800

    inserted the derivation of eatery CSV file

--- lib/screens/Eateries/FileReader.dart
+++ lib/screens/Eateries/FileReader.dart
@@ -2,16 +2,16 @@ import 'dart:io';
 import 'package:csv/csv.dart' as csv;
 import 'HealthyEateryRecommender.dart';
 
-// void main() {
-//   double radiusInKm = 3;
-//   List<Map<String, dynamic>> healthyEateryList =
-//       csvToDict('EateryData/HealthyEateries.csv');
-//   List<Map<String, dynamic>> withinRadiusEateries = filterEateryByRadius(
-//       healthyEateryList, 1.3527401672849244, 103.69683848785874, radiusInKm);
-//   List<Map<String, dynamic>> sampledEateries =
-//       sampleEateries(healthyEateryList, max: 6);
-//   print(sampledEateries);
-// }
+void main() {
+  double radiusInKm = 3;
+  List<Map<String, dynamic>> healthyEateryList =
+      csvToDict('EateryData/HealthyEateries.csv');
+  List<Map<String, dynamic>> withinRadiusEateries = filterEateryByRadius(
+      healthyEateryList, 1.3527401672849244, 103.69683848785874, radiusInKm);
+  List<Map<String, dynamic>> sampledEateries =
+      sampleEateries(healthyEateryList, max: 6);
+  print(sampledEateries);
+}
 
 // returns all eateries in csv file - list of dict
 List<Map<String, dynamic>> csvToDict(String filePath) {
