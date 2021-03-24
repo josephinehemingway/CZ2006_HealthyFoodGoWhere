b7c36b878e488529428fdeb84a83761353a097db
katelynn888 <74416901+katelynn888@users.noreply.github.com>
2021-03-25 01:47:38 +0800

    recipe detail page

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/screens/Recipe/Recipe.dart';
 import 'package:flutter_app/screens/Recipe/RecipeCard.dart';
+import 'package:flutter_app/screens/Recipe/RecipeDetails.dart';
 import 'package:http/http.dart' as http;
 import 'dart:math';
 
@@ -39,6 +40,35 @@ class ApiService {
     if (response.statusCode == 200) {
       final jsonRecipe = jsonDecode(response.body);
       return Recipe.fromJson(jsonRecipe);
+    } else {
+      throw Exception();
+    }
+  }
+  Future<RecipeDetails> getRecipeDetails(id) async {
+    final String _baseURL = "api.spoonacular.com";
+    const String API_KEY = "2f3c1b88a1e048819d9d57cad263dc04";
+
+    Map<String, String> parameters = {
+      'apiKey': API_KEY,
+      'includeNutrition': "true",
+    };
+
+    Uri uri = Uri.https(
+      _baseURL,
+      "/recipes/${id}/information",
+      parameters,
+    );
+
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    };
+
+    final response = await http.get(uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      final jsonRecipeDetails = jsonDecode(response.body);
+      RecipeDetails recipeDetails = RecipeDetails.fromJson(jsonRecipeDetails);
+      return recipeDetails;
     } else {
       throw Exception();
     }
