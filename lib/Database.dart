e56a410f5d1483e9aef7557f59e41d81581dcbed
katelynn888 <74416901+katelynn888@users.noreply.github.com>
2021-03-29 12:57:48 +0800

    saving into realtime database

---
+++ lib/Database.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_app/auth.dart';
+import 'CurrentUser.dart';
+final databaseReference = FirebaseDatabase.instance.reference();
+class Database{
+  static Future<String> createPreferences(List dietList) async{
+    String accountKey = await _getAccountKey(getUserID());
+    List preference = dietList;
+    var preferences = <String, dynamic>{
+      'preferences' : preference
+    };
+
+    DatabaseReference reference = FirebaseDatabase.instance
+      .reference()
+      .child("User")
+      .child(accountKey);
+
+    reference.set(preferences);
+    return reference.key;
+  }
+  static Future<void> savePreferences(CurrentUser user, List preferences) async{
+    String accountKey = await _getAccountKey(user.id);
+    return FirebaseDatabase.instance
+        .reference()
+        .child("User")
+        .child(accountKey)
+        .child("preferences")
+        .set(preferences);
+
+  }
+}
+Future<String> _getAccountKey(String id) async{
+  return id;
+}
